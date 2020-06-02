1d66565202226091c4e59d0e6ba43989cf01058b
Eryk Jaszkiewicz <eryk321-11@o2.pl>
2020-06-02 18:55:21 +0200

    new file: eryk

--- webpack.common.js
+++ webpack.common.js
@@ -63,5 +63,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/eryk.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "eryk.html",
+    }),
   ],
 };
